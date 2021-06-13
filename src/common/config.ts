104062dcc03b8538f29fc5dc7324b7e4d25973e2
Dzmitry Kosmach <dzmitry.kosmach@gmail.com>
2021-06-13 15:20:19 +0300

    feat: task6 docker

--- src/common/config.ts
+++ src/common/config.ts
@@ -5,6 +5,7 @@ dotenv.config({
   path: path.join(__dirname, '../../.env')
 });
 
-export const {PORT, NODE_ENV, MONGO_CONNECTION_STRING, JWT_SECRET_KEY, } = process.env;
+export const {PORT, PORT_HOST, POSTGRES_PORT, POSTGRES_USER, POSTGRES_PASSWORD,
+  POSTGRES_DBNODE_ENV, MONGO_CONNECTION_STRING, JWT_SECRET_KEY, } = process.env;
 const {authMode} =  process.env;
 export const AUTH_MODE = authMode === 'true';
