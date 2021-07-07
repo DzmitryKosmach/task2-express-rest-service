13b02d5e357fd65c9e1f030f728e8effa4c31e70
Dzmitry Kosmach <dzmitry.kosmach@gmail.com>
2021-07-07 13:48:14 +0300

    fix: logging error

--- src/logger/logger.middleware.ts
+++ src/logger/logger.middleware.ts
@@ -1,47 +1,29 @@
-import {
-  Inject,
-  Injectable,
-  LoggerService,
-  NestMiddleware,
-} from '@nestjs/common';
+import { Inject, Injectable, NestMiddleware } from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
 import { Logger } from 'winston';
 
 // Проверить implements LoggerService и  extends Logger from '@nestjs/common'
 @Injectable()
-export class LoggerMiddleware implements NestMiddleware, LoggerService {
+export class LoggerMiddleware implements NestMiddleware {
   constructor(
     @Inject('winston')
     private readonly logger: Logger,
   ) {}
 
-  log(message: any, context?: string) {
-    throw new Error('Method not implemented.');
-  }
-  error(message: any, trace?: string, context?: string) {
-    throw new Error('Method not implemented.');
-  }
-  warn(message: any, context?: string) {
-    throw new Error('Method not implemented.');
-  }
-  debug?(message: any, context?: string) {
-    throw new Error('Method not implemented.');
-  }
-  verbose?(message: any, context?: string) {
-    throw new Error('Method not implemented.');
-  }
-
   use(req: Request, res: Response, next: NextFunction) {
-    this.logger.info(
-      `url: ${JSON.stringify(req.baseUrl)} queryParams: ${JSON.stringify(
-        req.query,
-      )} pathParams: ${JSON.stringify(req.params)} body: ${JSON.stringify(
-        req.body,
-      )} statusCode: ${res.statusCode}`,
-    );
-    if (Number(res.status) > 399) {
-      this.logger.error(`Error ${res.status}`);
-    }
+    const message = `url: ${req.baseUrl}; queryParams: ${JSON.stringify(
+      req.query,
+    )}; pathParams: ${JSON.stringify(req.params)}; body: ${JSON.stringify(
+      req.body,
+    )}; statusCode: ${res.statusCode}`;
+
+    res.on('finish', () => {
+      this.logger.info(`Level: INFO: ${message}`);
+      if (res.statusCode > 399) {
+        this.logger.error(`Level: ERROR: ${message}`);
+      }
+    });
+
     next();
   }
 }
